f8944852d03d7fac3ddec4be89e1952e389f5baa
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2021-08-22 00:19:28 +0100

    Refactor movement and animation

--- client/scripts/singleplayer/player_sp.gd
+++ client/scripts/singleplayer/player_sp.gd
@@ -4,34 +4,23 @@ export var max_health : int = 5
 var current_health : int
 
 export var speed : int = 100
-var direction : Vector2
 
 onready var sprite = $AnimatedSprite
 
 func _ready():
 	current_health = max_health
-	direction = Vector2()
 
 func _process(delta):
-	poll_movement()
-
+	var direction: Vector2 = poll_movement()
 	var velocity : Vector2 = Vector2(direction.x * speed, direction.y * speed)
 	velocity.normalized()
 
 	move_and_slide(velocity)
+	set_animation(direction)
 
-	if direction.x > 0:
-		set_animation("MoveRight")
-	elif direction.x < 0:
-		set_animation("MoveLeft")
-	elif direction.y > 0:
-		set_animation("MoveBottom")
-	elif direction.y < 0:
-		set_animation("MoveTop")
-	else:
-		set_animation("IdleBottom")
+func poll_movement() -> Vector2:
+	var direction : Vector2 = Vector2()
 
-func poll_movement() -> void:
 	# horizontal
 	if Input.is_action_pressed("move_left"):
 		direction.x = -1
@@ -48,6 +37,20 @@ func poll_movement() -> void:
 	else:
 		direction.y = 0
 
-func set_animation(animation_name):
+	return direction
+
+func set_animation(direction: Vector2) -> void:
+	if direction.x > 0:
+		set_animation_by_name("MoveRight")
+	elif direction.x < 0:
+		set_animation_by_name("MoveLeft")
+	elif direction.y > 0:
+		set_animation_by_name("MoveBottom")
+	elif direction.y < 0:
+		set_animation_by_name("MoveTop")
+	else:
+		set_animation_by_name("IdleBottom")
+
+func set_animation_by_name(animation_name) -> void:
 	if sprite.animation != animation_name:
 		sprite.play(animation_name)
