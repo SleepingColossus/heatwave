4b9214d52a37a6b6026dc7ff8769c728bc83314b
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2021-08-28 19:44:37 +0100

    Simplify waves

--- client/scripts/singleplayer/waves.gd
+++ client/scripts/singleplayer/waves.gd
@@ -3,14 +3,13 @@ class_name Waves
 const wave_data : Dictionary = {
 		1: {
 			EnemyType.EnemyType.MELEE_BASIC: 1,
-			#EnemyType.EnemyType.MELEE_FAST: 1,
-			#EnemyType.EnemyType.RANGED_BASIC: 1,
-			#EnemyType.EnemyType.RANGED_ADVANCED: 1,
-			#EnemyType.EnemyType.TANK: 1,
 		},
 		2: {
-			EnemyType.EnemyType.MELEE_BASIC: 2,
+			EnemyType.EnemyType.MELEE_BASIC: 1,
 			EnemyType.EnemyType.MELEE_FAST: 1,
+			EnemyType.EnemyType.RANGED_BASIC: 1,
+			EnemyType.EnemyType.RANGED_ADVANCED: 1,
+			EnemyType.EnemyType.TANK: 1,
 		},
 	}
 
