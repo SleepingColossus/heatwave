224db7c0e2206b457445d11de6858e3ae514db2a
Aleksandar Mancic <AcaMancic@users.noreply.github.com>
2021-10-11 20:26:40 +0100

    Add 10 waves

--- client/scripts/gameplay/waves.gd
+++ client/scripts/gameplay/waves.gd
@@ -5,11 +5,54 @@ const wave_data : Dictionary = {
 			EnemyType.EnemyType.MELEE_BASIC: 1,
 		},
 		2: {
-			EnemyType.EnemyType.MELEE_BASIC: 1,
+			EnemyType.EnemyType.MELEE_BASIC: 4,
+			EnemyType.EnemyType.RANGED_BASIC: 2,
+
+		},
+		3: {
+			EnemyType.EnemyType.MELEE_BASIC: 4,
+			EnemyType.EnemyType.RANGED_BASIC: 4,
+			EnemyType.EnemyType.TANK: 1,
+		},
+		4: {
+			EnemyType.EnemyType.MELEE_BASIC: 6,
 			EnemyType.EnemyType.MELEE_FAST: 1,
-			EnemyType.EnemyType.RANGED_BASIC: 1,
+			EnemyType.EnemyType.RANGED_BASIC: 2,
 			EnemyType.EnemyType.RANGED_ADVANCED: 1,
 			EnemyType.EnemyType.TANK: 1,
+		},
+		5: {
+			EnemyType.EnemyType.MELEE_BASIC: 10,
+			EnemyType.EnemyType.MELEE_FAST: 3,
+			EnemyType.EnemyType.RANGED_BASIC: 5,
+			EnemyType.EnemyType.TANK: 2,
+		},
+		6: {
+			EnemyType.EnemyType.MELEE_BASIC: 5,
+			EnemyType.EnemyType.MELEE_FAST: 3,
+			EnemyType.EnemyType.RANGED_BASIC: 10,
+			EnemyType.EnemyType.RANGED_ADVANCED: 2,
+			EnemyType.EnemyType.TANK: 2,
+		},
+		7: {
+			EnemyType.EnemyType.MELEE_BASIC: 20,
+			EnemyType.EnemyType.RANGED_BASIC: 7,
+			EnemyType.EnemyType.RANGED_ADVANCED: 3,
+			EnemyType.EnemyType.TANK: 1,
+		},
+		8: {
+			EnemyType.EnemyType.RANGED_BASIC: 10,
+			EnemyType.EnemyType.RANGED_ADVANCED: 5,
+			EnemyType.EnemyType.TANK: 5,
+		},
+		9: {
+			EnemyType.EnemyType.MELEE_BASIC: 15,
+			EnemyType.EnemyType.RANGED_BASIC: 15,
+			EnemyType.EnemyType.RANGED_ADVANCED: 5,
+			EnemyType.EnemyType.TANK: 5,
+		},
+		10: {
+			EnemyType.EnemyType.MELEE_FAST: 20,
 		},
 	}
 
