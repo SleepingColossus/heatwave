b6b87908a9c5bd9f4158eca35b3dee2bd1a0eeb8
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2021-10-12 20:55:06 +0100

    Pickups despawn after 5 seconds

--- client/scripts/gameplay/medkit.gd
+++ client/scripts/gameplay/medkit.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 export var healing_amount : int = 1
-onready var pickup_sound : AudioStreamPlayer2D = $PickupSound
 onready var collect_animation : AnimationPlayer = $CollectAnimation
+onready var despawn_animation: AnimationPlayer = $DespawnAnimation
 var is_collected: bool = false
 
 func _on_HealthPickUp_body_entered(body):
@@ -10,3 +10,6 @@ func _on_HealthPickUp_body_entered(body):
 		is_collected = true
 		body.heal(healing_amount)
 		collect_animation.play("Collected")
+
+func _on_DespawnTimer_timeout():
+	despawn_animation.play("Despawned")
--- client/scripts/gameplay/weapon_pickup.gd
+++ client/scripts/gameplay/weapon_pickup.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 export var weapon_type: int
 onready var collect_animation: AnimationPlayer = $CollectAnimation
+onready var despawn_animation: AnimationPlayer = $DespawnAnimation
 var is_collected: bool = false
 
 func _on_collected(body):
@@ -9,3 +10,6 @@ func _on_collected(body):
 		is_collected = true
 		body.change_weapon(weapon_type)
 		collect_animation.play("Collected")
+
+func _on_DespawnTimer_timeout():
+	despawn_animation.play("Despawned")
