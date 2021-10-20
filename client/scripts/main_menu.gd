2d6112b6a8e254b654387504026c5c15d2935346
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2021-10-20 20:44:00 +0100

    Add animated player to main menu

--- client/scripts/main_menu.gd
+++ client/scripts/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+onready var sprite: AnimatedSprite = $Menu/CenterRow/AnimationContainer/AnimatedSprite
+
 func _ready():
 	for button in $Menu/CenterRow/Buttons.get_children():
 		button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
@@ -9,3 +11,12 @@ func _on_button_pressed(scene_to_load):
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _on_PlayButton_mouse_entered():
+	sprite.play("MoveLeft")
+
+func _on_HelpButton_mouse_entered():
+	sprite.play("IdleBottom")
+
+func _on_QuitButton_mouse_entered():
+	sprite.play("Dying")
